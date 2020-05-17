97e953a258dd8c9c1097a1b80aac67509e31de7f
bramhesh <bramhesh.srivastav@algo8.ai>
2020-05-17 10:23:34 +0530

    Initial commit

---
+++ Send Text Message Over Whatsapp/script.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import pandas as pd
+options = webdriver.ChromeOptions()
+options.add_argument("--start-maximized")
+driver = webdriver.Chrome(chrome_options=options)
+df = pd.DataFrame(columns=['Case No','Age','Nationality','Area'])
+driver.get("https://web.whatsapp.com/")
+time.sleep(10)
+driver.find_element(By.XPATH,'//*[@id="side"]/div[1]/div/label/div/div[2]').send_keys('Name of the person whom you want to send text')
+time.sleep(5)
+driver.find_element(By.XPATH,'//*[@id="pane-side"]/div[1]/div/div/div[1]/div/div/div[2]').click()
+for i in range(1):
+    driver.find_element(By.XPATH,'//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys('*Hi This is Bramhesh*')
+    driver.find_element(By.XPATH,'//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys(Keys.SHIFT,Keys.ENTER)
+    driver.find_element(By.XPATH,'//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys('*This is example text*')
+    driver.find_element(By.XPATH,'//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys(Keys.SHIFT,Keys.ENTER)
+    driver.find_element(By.XPATH,'//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys('Bye')
+    driver.find_element(By.XPATH,'//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys(Keys.ENTER)
